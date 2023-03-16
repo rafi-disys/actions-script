dc3bb86a2c608e3ae50619cb13d780368d51693c
rafi-disys <124764810+rafi-disys@users.noreply.github.com>
2023-03-16 05:46:01 -0700

    hoping this program would work

--- bash-script.sh
+++ bash-script.sh
@@ -1,5 +1,33 @@
-while true; do
-	rand=$(shuf -i 2600-2700 -n 1)
-	echo -en "   \u$rand"
-	sleep 1
+# Program in Bash to
+# print pyramid
+ 
+# Static input to the
+# number
+p=7;
+ 
+for((m=1; m<=p; m++))
+do
+    # This loop print spaces
+    # required
+    for((a=m; a<=p; a++))
+    do
+      echo -ne " ";
+    done
+     
+    # This loop print the left
+    # side of the pyramid
+    for((n=1; n<=m; n++))
+    do
+      echo -ne "#";
+    done
+ 
+    # This loop print right
+    # side of the pyramid.
+    for((i=1; i<m; i++))
+    do
+      echo -ne "#";
+    done
+ 
+    # New line
+    echo;
 done
