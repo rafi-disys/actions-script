cb473189ae83593698e95c03cb4191c781b19d96
rafi-disys <124764810+rafi-disys@users.noreply.github.com>
2023-03-16 05:34:32 -0700

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-MAX_NO=0
-
-echo -n "Enter Number between (5 to 9) : "
-read MAX_NO
+MAX_NO=7
 
 if ! [ $MAX_NO -ge 5 -a $MAX_NO -le 9 ]; then
 	echo "WTF... I ask to enter number between 5 and 9, Try Again"
