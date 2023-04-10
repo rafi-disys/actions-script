698a6afad9166fc7c6ef26a11f79e01ca88397a8
rafi-disys <124764810+rafi-disys@users.noreply.github.com>
2023-04-10 19:16:12 +0530

    added if condition to check if directory exists

--- bash-script.sh
+++ bash-script.sh
@@ -3,4 +3,12 @@
 # cd passwords
 # echo "username= rafi password= " > password.txt
 
-cat $GITHUB_WORKSPACE
+PASS_DIR = /home/azureuser/actions-runner/_work/actions-script/actions-script/passwords
+if [ -d "$PASS_DIR" ];
+then
+    echo "$PASS_DIR does not exist \n Creating the directory..."
+    mkdir passwords && touch password.txt
+else
+	echo "username = rafi password = $password" >> password.txt
+fi
+
